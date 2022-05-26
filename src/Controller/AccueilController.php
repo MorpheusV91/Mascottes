63315f5c56088c45ecd1030dc38ec44fb7fa360d
gibralta fabrice <gibralta.fabrice@gmx.fr>
2022-05-26 21:09:37 +0200

    changement de du controller et du template Home

---
+++ src/Controller/AccueilController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AccueilController extends AbstractController
+{
+    /**
+     * @Route("/accueil", name="accueil")
+     * @Route("/")
+     */
+    public function accueil(): Response
+    {
+        return $this->render('accueil.html.twig', [
+            'controller_name' => 'AccueilController',
+        ]);
+    }
+}
