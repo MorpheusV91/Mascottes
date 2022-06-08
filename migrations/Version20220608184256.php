3ad3c2adb7e2562957385692d363b1c32689290a
gibralta fabrice <gibralta.fabrice@gmx.fr>
2022-06-08 20:52:00 +0200

    table moyen de paiement et mise en BDD

---
+++ migrations/Version20220608184256.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220608184256 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE moyen_de_paiement CHANGE date_de_sortie date_ajout DATE NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE moyen_de_paiement CHANGE date_ajout date_de_sortie DATE NOT NULL');
+    }
+}
